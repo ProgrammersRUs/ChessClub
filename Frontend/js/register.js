05bcd15e979980ea7ed90ba78ef71e0a6482eff8
August <Augusthauerslev@outlook.com>
2022-05-03 15:21:09 +0200

    JS updated

--- Frontend/js/register.js
+++ Frontend/js/register.js
@@ -1,11 +1,56 @@
-const newMember = "http://localhost:8080/register"
+function out(message){
+    console.log(message)
+}
 
-async function registerMember(){
-    return fetch(newMember).then(response => response.json())
+let memberForm
+
+document.addEventListener('DOMContentLoaded', createFormEventListener)
+
+function createFormEventListener(){
+    memberForm = document.getElementById('newMember')
+    memberForm.addEventListener('submit', formHandleSubmit)
+}
+
+async function formHandleSubmit(registerData){
+    registerData.preventDefault();
+    const form = registerData.currentTarget;
+    const url = form.action;
+
+    try{
+        const formData = new FormData(form);
+        if (validateMemberData(registerData)){
+            await postFormDataAsJson(url, formData)
+        }
+    }catch (err){
+        alert(err.message)
+        out(err);
+    }
+}
+
+function validateMemberData(registerData){
+alert("Insert validation here")
 }
 
 
+//Er ikke sikker på jeg egentligt forstår denne her men den har virket for det før - August
+async function postFormDataAsJson(url, formData) {
+    out(formData.entries());
+    const plainFormData = Object.fromEntries(formData.entries());
+    out(plainFormData);
 
-function validateMemberData(){
+    const formDataJsonString = JSON.stringify(plainFormData);
 
+    const fetchOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: formDataJsonString
+    };
+
+    const response = await fetch(url, fetchOptions);
+    if (!response) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
 }
