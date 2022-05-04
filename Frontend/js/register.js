e7d85f4c8c20e339e3d47564295a10a125213b0f
August <Augusthauerslev@outlook.com>
2022-05-04 10:34:20 +0200

    Age sættes nu med dato og bliver konverteret til String

--- Frontend/js/register.js
+++ Frontend/js/register.js
@@ -7,6 +7,19 @@ function createFormEventListener() {
     memberForm.addEventListener('submit', handleFormSubmit)
 }
 
+$(document).ready(function () {
+    var date_input = $('input[name="memberAge"]'); //our date input has the name "date"
+    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    var options = {
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    };
+    date_input.datepicker(options);
+
+})
+
 async function handleFormSubmit(event) {
     event.preventDefault();
     const form = event.currentTarget;
