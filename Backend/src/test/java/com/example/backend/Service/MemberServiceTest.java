721a741bee2917486089c0a3e9872d00dc4206cf
August <Augusthauerslev@outlook.com>
2022-05-04 14:32:10 +0200

    Startet tests

--- Backend/src/test/java/com/example/backend/Service/MemberServiceTest.java
+++ Backend/src/test/java/com/example/backend/Service/MemberServiceTest.java
@@ -2,10 +2,14 @@ package com.example.backend.Service;
 
 import com.example.backend.Entity.Member;
 import com.example.backend.Repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +17,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     @Autowired
-    private MemberRepository memberRepository;
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach(){
+
+
+    }
 
     @Test
     void saveMember() {
@@ -24,10 +34,9 @@ class MemberServiceTest {
 
         Member savedMember = memberRepository.getById(1);
 
-        assertEquals(savedMember.getMemberId(), member.getMemberId());
+        System.out.println(savedMember + " " + member);
 
-
-
+        assertEquals(savedMember,member);
     }
 
     @Test
@@ -38,10 +47,9 @@ class MemberServiceTest {
 
         Member foundID = memberRepository.getById(testUser.getMemberId());
 
-        System.out.println(foundID.getMemberId());
-
         assertEquals(foundID.getMemberId(),testUser.getMemberId());
     }
+
 
     @Test
     void findById() {
