637f42e786eead75fb39e5412de099140249d838
Kristian Kryger <kristianlaukryger@gmail.com>
2022-05-05 14:32:21 +0200

    Memberoverview er stylet og importere brugere fra databasen

---
+++ Frontend/js/memberoverview.js
@@ -0,0 +1,56 @@
+const member = 'http://localhost:8080/member/all-members';
+const memberOverview = document.getElementById('bview-table')
+
+function fetchMember() {
+    return fetch(member).then(response => response.json());
+}
+
+addTableOverview(member)
+    .catch(err => console.error(err));
+
+async function addTableOverview() {
+    const members = await fetchMember();
+    console.log(members)
+    const tbody = document.createElement('tbody')
+    tbody.setAttribute('id', 'tbodyadmin')
+
+    for (let member of members) {
+        for (let i = 0; i < members.length; i++) {
+            const tableParent = document.getElementById('parent')
+            const tableRow = document.createElement('tr');
+            const td1 = document.createElement('td');
+            td1.classList.add('pl-4')
+
+            td1.textContent = member.memberId;
+            const td2 = document.createElement('td');
+            td2.classList.add('')
+            td2.textContent = member.memberFirstName;
+            const td3 = document.createElement('td');
+            td3.classList.add('text-muted')
+            td3.textContent = member.memberLastName;
+            const td4 = document.createElement('td');
+            td4.classList.add('text-muted')
+            td4.textContent = member.memberEmail;
+            const td5 = document.createElement('td');
+            td5.classList.add('text-muted')
+            td5.textContent = member.memberPhoneNr;
+            const td6 = document.createElement('td');
+            td6.classList.add('text-muted')
+            td6.textContent = member.memberAge;
+            const td7 = document.createElement('td');
+            td7.classList.add('text-muted')
+            td7.textContent = member.memberAddress;
+
+            console.log("hej")
+            tableParent.append(tableRow);
+            tableRow.append(td1);
+            tableRow.append(td2);
+            tableRow.append(td3);
+            tableRow.append(td4);
+            tableRow.append(td5);
+            tableRow.append(td6);
+            tableRow.append(td7);
+            tbody.append(tableRow);
+        }
+    }
+}
--- Frontend/js/screens/member/main.js
+++ Frontend/js/screens/member/main.js
@@ -1,8 +1,13 @@
 import ElementObject from '../../lib/ElementObject.js';
 import NavbarComponent from '../../components/NavbarComponent.js';
+import MemberOverviewComponent from "../../components/MemberOverviewComponent.js";
 
+
+const memberOverview = new ElementObject('overview');
+memberOverview.addComponent(MemberOverviewComponent);
 
 const home = new ElementObject('navbar');
 home.addComponent(NavbarComponent);
 
 home.updateDOM();
+memberOverview.updateDOM();
