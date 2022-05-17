f0e4451e07b468f612f4f9ceb3b8ca3e71e52144
silke <silk2918@stud.kea.dk>
2022-05-17 19:38:52 +0200

    ting på samme linje. kinda

--- Frontend/js/components/MemberComponent.js
+++ Frontend/js/components/MemberComponent.js
@@ -12,10 +12,14 @@ class MemberComponent extends Component {
         super('nextEvent', state, (state) =>
             `
      
-    <section id="members" class="members d-none d-lg-block mb-2 bg-light">
+     <section id="members" class="members d-none d-lg-block mb-2 bg-light">
     <div class="container">
         <h3 class="mb-2">Mød medlemmerne</h3>
-      
+        <div class="carousel slide" id="memberCarousel" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                 
+   
                   ${this.renderMembers(state.month)}
 
       `)
@@ -25,25 +29,25 @@ class MemberComponent extends Component {
 
         return month.map(month => `
                               
-                   <div class="carousel slide" id="memberCarousel" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    
+               <div class="row">
                         <div class="col-lg-4 text-center mb-2">
                             <div class="d-flex justify-content-center">
-        <div style="height: 100px; width: 100px; background-color: green; border-radius: 25rem">
-                         <h5>${month.username}</h5>
-                           <!-- <p>Rating: 1253</p> -->
-                             <p>${month.joined}</p>
+                                <div style="height: 100px; width: 100px; background-color: green; border-radius: 25rem">
+                        
+                                </div>
+                            </div>
+                            <h5>Rasputin</h5>
+                            <p>Rating: 1253</p>
+                            <p>"Hyggelig klub med dejlige mennesker"</p>
                         </div>
-                  
-                  </div>
                     </div>
                 </div>
-                </div>
-            
+            </div>
+        </div>
     </div>
-</section>    
+</section>
+     
+     
       
         `
         ).join('');
