d4d65b445ab2b0b23137720c6b468c760283910b
silke <silk2918@stud.kea.dk>
2022-05-18 00:21:23 +0200

    links til medlemmer

--- Frontend/js/components/MemberComponent.js
+++ Frontend/js/components/MemberComponent.js
@@ -13,35 +13,41 @@ class MemberComponent extends Component {
             `
             <section id="members" class="mb-1 mt-1">
  
-              
         <h3 class="mb-2">Mød medlemmerne</h3>
         <div class="d-flex justify-content-center">
+        
    ${this.renderMembers(state.month)}
-
+   
+</div>
 </section>
       `)
     }
 
-    renderMembers(month) {
+  renderMembers(month) {
 
-        return month.map(month => `
+   return month.map(month => `
 
-      
-                     <div style="display: inline-block">
-                        <div class="col-lg-12 pt-4 pt-lg-0 ">
-                            <div class="d-flex justify-content-center">
-                                <div style="height: 100px; width: 100px; background-color: green; border-radius: 25rem">
+     <div style="display: inline-block">
+       <div class="col-lg-12 pt-4 pt-lg-0 ">
+         <div class="d-flex justify-content-center">
+         <div class="container">
+         
+           <div style="height: 100px; width: 100px; background-color: green; border-radius: 25rem">
                        
-                                </div>
-                            </div>
-                            <div class="container">
-                            <h5>${month.username}</h5>
-                           <!-- <p>Rating: 1253</p>-->
-                            <p>${month.joined}</p>
-                        </div>
-                    </div>
-                    </div>
-          
+           </div>
+         </div>
+         
+         <div class="container">
+         <br>
+         <a href="https://www.chess.com/member/${month.username}">
+         <h5>${month.username}</h5>
+         </a>
+          <!-- <p>Rating: 1253</p>-->
+          <p>${month.joined}</p>
+          </div>
+       </div>
+     </div>
+     </div>
 `
         ).join('');
     }
