88b65a2ef96bb9e3e82260a44dfe990468d9bf90
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-05-17 16:34:39 +0200

    added frontpage controller

--- CMS/src/main/java/com/example/cms/controller/FrontPageController.java
+++ CMS/src/main/java/com/example/cms/controller/FrontPageController.java
@@ -1,4 +1,70 @@
 package com.example.cms.controller;
 
+import com.example.cms.entity.FrontPage;
+import com.example.cms.service.FrontPageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping("front-page")
 public class FrontPageController {
+
+  @Autowired
+  FrontPageService frontPageService;
+
+  @PostMapping("/new")
+  @ResponseStatus(HttpStatus.CREATED)
+  public FrontPage postFrontPage(@RequestBody FrontPage frontPage) {
+    return frontPageService.addNew(frontPage);
+  }
+
+  @GetMapping("/{id}")
+  public FrontPage findById(@PathVariable int id) {
+    return frontPageService.getFrontPageById(id);
+  }
+
+  @GetMapping("/all-fronpages")
+  public List<FrontPage> getAllFrontPages() {
+    return frontPageService.getAllFrontPages();
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<FrontPage> FrontPage(@PathVariable int id, @RequestBody FrontPage frontPage){
+    Optional<FrontPage> optFrontPage = frontPageService.findByisOpt(id);
+    if (optFrontPage.isPresent()){
+      frontPageService.addNew(frontPage);
+      return  new ResponseEntity<>(frontPage, HttpStatus.OK);
+    } else {
+      FrontPage frontPageNotFound = new FrontPage();
+      frontPageNotFound.setHeader("No frontPage with id: " + id);
+      return  new ResponseEntity<>(frontPage, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteFrontPage(@PathVariable int id) {
+    try {
+      frontPageService.deleteFrontPage(id);
+      return new ResponseEntity<>("FrontPage Deleted with id: " + id, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+  }
+
 }
--- CMS/src/main/java/com/example/cms/service/FrontPageService.java
+++ CMS/src/main/java/com/example/cms/service/FrontPageService.java
@@ -1,8 +1,12 @@
 package com.example.cms.service;
 
+import com.example.cms.entity.FrontPage;
 import com.example.cms.repository.FrontPageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FrontPageService {
@@ -10,4 +14,23 @@ public class FrontPageService {
   @Autowired
   FrontPageRepository frontPageRepository;
 
+  public FrontPage addNew(FrontPage frontPage) {
+    return frontPageRepository.save(frontPage);
+  }
+
+  public FrontPage getFrontPageById(int id) {
+    return frontPageRepository.findById(id).get();
+  }
+
+  public List<FrontPage> getAllFrontPages() {
+    return frontPageRepository.findAll();
+  }
+
+  public Optional<FrontPage> findByisOpt(int id) {
+    return frontPageRepository.findById(id);
+  }
+
+  public void deleteFrontPage(int id) {
+    frontPageRepository.deleteById(id);
+  }
 }
