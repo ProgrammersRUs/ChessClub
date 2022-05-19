a5a796b3f51ead87767e05b7aa8f4e6b533dbbbb
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-05-19 20:22:45 +0200

    added dynamic content frontpage

--- Frontend/js/components/cms/tables/SponsorTableComponent.js
+++ Frontend/js/components/cms/tables/SponsorTableComponent.js
@@ -23,7 +23,6 @@ class SponsorTableComponent extends Component {
                         </tbody>
                     </table>
                 </div>
-
             </div>
 
   
--- Frontend/js/screens/home/main.js
+++ Frontend/js/screens/home/main.js
@@ -12,20 +12,25 @@ navbar.addComponent(new NavbarComponent());
 
 const body = new ElementObject('test');
 
-let image1 = new ImageComponent("aboutImage", "./img/Haslev%20og%20Faxe%20Skakklub%20-logos_black.png",
-    "...");
-let about = new TextComponent("about", "Om os", `<p
-            className="fst-italic"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.</p>`);
-
-let column1 = new twoColumnComponent('1', about, image1);
 let frontPageNews = new FrontPageNewsWrapperComponent(new NewsComponent(await
     fetch(config.endpoints.cms.root+config.endpoints.cms.subPoint.allNews).then(response => response.json())));
 body.addComponent(frontPageNews)
-body.addComponent(column1);
 
 const footer = new ElementObject('footer');
 footer.addComponent(new FooterComponent());
+
+let frontpageContent = await
+    fetch(config.endpoints.cms.root+config.endpoints.cms.subPoint.allFrontPages).then(response => response.json());
+
+for (let content of frontpageContent){
+    let image1 = new ImageComponent("aboutImage", content.imageUrl,
+        "...");
+    let about = new TextComponent("about", content.header, `<p
+            className="fst-italic"> ${content.body}</p>`);
+
+    let column1 = new twoColumnComponent('1', about, image1);
+    body.addComponent(column1)
+}
 
 
 navbar.updateDOM();
