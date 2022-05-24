a120209803e42ef0c7cce3d1c4241578ba92ffff
Andreas Simmelkiaer <andreas.sim@live.dk>
2022-05-24 15:14:14 +0200

    Removed dead imports

--- CMS/src/main/java/com/example/cms/service/ImageBlobService.java
+++ CMS/src/main/java/com/example/cms/service/ImageBlobService.java
@@ -1,19 +1,14 @@
 package com.example.cms.service;
 
 import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.specialized.BlockBlobClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.WritableResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 @Service
 public class ImageBlobService {
