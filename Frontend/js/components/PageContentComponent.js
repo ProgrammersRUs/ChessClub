de5ba0a36dc0e7124f9a9384d258d1db21dc8b1d
silke <silk2918@stud.kea.dk>
2022-05-26 00:41:41 +0200

    velkomst tekst er pænere

--- Frontend/js/components/PageContentComponent.js
+++ Frontend/js/components/PageContentComponent.js
@@ -13,23 +13,30 @@ class PageContentComponent extends Component{
     renderNews(content) {
 
         return content.filter(post => post.isActive == true).map(post =>  `
-<div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading">${post.header} <span class="text-muted"></span></h2>
-            <p class="lead">${post.body}</p>
 
-        </div>
-        <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto"
 
-                    alt="500x500" style="width: 500px; height: 500px;"
-                 src="${post.imgUrl}"
-                   data-holder-rendered="true"
-                width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade">
-        </div>
+
+<div class="container marketing mt-3" id="myContent">
+ <div class="container"> <!--original -->
+  <div class="col-md-12">
+   <div style="margin-top: 5px">
+    <h2 class="featurette-heading">${post.header} <span class="text-muted"></span></h2>
+   </div>
+   <div class="row featurette">
+    <div class="col-md-7">
+     <p class="lead">${post.body}</p>
     </div>
-        <hr class="featurette-divider">
+    <div class="col-md-5">
+     <img class="featurette-image img-fluid mx-auto" alt="500x500" style="width: 500px; height: 500px;" src="${post.imgUrl}"
+     data-holder-rendered="true" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+     referrerpolicy="no-referrer-when-downgrade">
+    </div>
+   </div>
+   <hr class="featurette-divider">
+  </div>
+ </div>
+</div>
+
 `
         ).join('')
     }
